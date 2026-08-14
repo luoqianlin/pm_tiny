d0f095e9ddb858ab284b12d70d29b645a5dee4f6
luoqianlin <qianlinluo@foxmail.com>
2026-08-14 14:12:41 +0800

    revert: retain Windows pipe queue capacity

--- src/platform/windows/asio_named_pipe.cpp
+++ src/platform/windows/asio_named_pipe.cpp
@@ -13,10 +13,7 @@
 
 namespace pm_tiny { namespace win {
 
-namespace {
-// Keep a stalled named-pipe client from retaining an unbounded log backlog.
-constexpr std::size_t session_queue_limit = 64U * 1024U;
-}
+namespace { constexpr std::size_t session_queue_limit = 1024U * 1024U; }
 
 namespace {
 
